aeb11ceceb0a48f1a4e701d95e82a4ae1e9adc21
Thiago Souza da Rosa <thiagosouzadarosa@gmail.com>
2021-02-03 15:29:29 -0300

    Update 03_fix_start.py

--- Desafios/03_fix_start.py
+++ Desafios/03_fix_start.py
@@ -14,8 +14,8 @@ onde todas as instancias de stra foram substituidas por strb.
 """
 
 def fix_start(s):
-    # +++ SUA SOLUÇÃO +++
-    return
+    # solução
+    return s[0] + s[1:].replace(s[0], '*')
 
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
