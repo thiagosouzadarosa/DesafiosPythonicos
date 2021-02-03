1e927a3e7b83bb69d9379ec609a3aab6d634dbfe
Thiago Souza da Rosa <thiagosouzadarosa@gmail.com>
2021-02-03 11:38:08 -0300

    Update 01_donuts.py

--- 01_donuts.py
+++ 01_donuts.py
@@ -10,8 +10,11 @@ e donuts(23) retorna 'Number of donuts: many'
 """
 
 def donuts(count):
-    # +++ SUA SOLUÇÃO +++
-    return
+    if count < 10:
+       donuts = str(count)
+    else:
+        donuts = 'many'
+    return 'Number of donuts: ' + donuts
 
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
